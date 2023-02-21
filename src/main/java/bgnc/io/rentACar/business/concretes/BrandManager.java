4ce7f61112f75b5f8497637442e26111856d0db6
Bugra <71045060+TheGnc@users.noreply.github.com>
2023-02-21 19:10:43 +0300

    mapper

--- src/main/java/bgnc/io/rentACar/business/concretes/BrandManager.java
+++ src/main/java/bgnc/io/rentACar/business/concretes/BrandManager.java
@@ -3,44 +3,42 @@ package bgnc.io.rentACar.business.concretes;
 import bgnc.io.rentACar.business.abstracts.BrandService;
 import bgnc.io.rentACar.business.requests.CreateBrandRequest;
 import bgnc.io.rentACar.business.responses.GetAllBrandsResponse;
+import bgnc.io.rentACar.core.utilities.mappers.ModelMapperService;
 import bgnc.io.rentACar.dataAccess.abstracts.BrandRepository;
 import bgnc.io.rentACar.model.concretes.Brand;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class BrandManager implements BrandService {
 
     private BrandRepository brandRepository;
-
-    @Autowired
-    public BrandManager(BrandRepository brandRepository) {
-        this.brandRepository = brandRepository;
-    }
-
+    private ModelMapperService modelMapperService;
 
     @Override
     public List<GetAllBrandsResponse> getAll() {
 
         List<Brand> brands = brandRepository.findAll();
-        List<GetAllBrandsResponse> brandsResponses=new ArrayList<>();
-        for (Brand brand:brands){
+        List<GetAllBrandsResponse> brandsResponses=brands.
+                stream()
+                .map(brand ->
+                        this.modelMapperService.
+                                forResponse().
+                                map(brand,GetAllBrandsResponse.class)).toList();
 
-            GetAllBrandsResponse responseItem = new GetAllBrandsResponse();
-            responseItem.setId(brand.getId());
-            responseItem.setName(brand.getName());
-            brandsResponses.add(responseItem);
-        }
         return  brandsResponses;
     }
 
     @Override
     public void add(CreateBrandRequest createBrandRequest) {
-        Brand brand = new Brand();
-        brand.setName(createBrandRequest.getName());
+        Brand brand = this.modelMapperService.
+                forRequest().
+                map(createBrandRequest,Brand.class);
         this.brandRepository.save(brand);
     }
 }
--- src/main/java/bgnc/io/rentACar/webApi/controllers/BrandsController.java
+++ src/main/java/bgnc/io/rentACar/webApi/controllers/BrandsController.java
@@ -3,8 +3,8 @@ package bgnc.io.rentACar.webApi.controllers;
 import bgnc.io.rentACar.business.abstracts.BrandService;
 import bgnc.io.rentACar.business.requests.CreateBrandRequest;
 import bgnc.io.rentACar.business.responses.GetAllBrandsResponse;
-import bgnc.io.rentACar.model.concretes.Brand;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,14 +13,10 @@ import java.util.List;
  **/
 @RestController
 @RequestMapping("/api/brands")
+@AllArgsConstructor
 public class BrandsController {
 
     private BrandService brandService;
-
-    @Autowired
-    public BrandsController(BrandService brandService) {
-        this.brandService = brandService;
-    }
 
     @GetMapping("/getall")
     public List<GetAllBrandsResponse> getAll(){
